a21876c7ffca648ed2cb3a9ee64433a119f623c4
PeterLeSouchu <peter22510@gmail.com>
2024-07-02 16:48:48 +0200

    feat: add nav-bottom's app with sticky position on all page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,92 @@
 function Footer() {
   return (
-    <footer className="footer footer-center bg-blue/50 text-base-content text-black p-4">
+    <footer className="footer footer-center bg-blue/50 text-base-content text-black p-4 h-max">
       <aside>
         <p>
           Copyright © {new Date().getFullYear()} - CityZen - Tous droits
           réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Copyright © {new Date().getFullYear()} - CityZen - Tous droits
+          réservés
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic rerum
+          harum sequi voluptate voluptates assumenda dolores delectus. Suscipit
+          ipsum cumque incidunt sunt, quos odit quae. Cumque voluptas vero velit
+          recusandae!
         </p>
       </aside>
     </footer>
