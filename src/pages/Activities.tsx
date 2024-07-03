cbf6bdbd2454a75027bc9c4cb10e087ef89df9da
ziad <ziadelidrissi1@gmail.com>
2024-07-03 15:46:59 +0200

    feat: add basic lealfet map

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,5 +1,14 @@
+import Map from '../components/Map';
+
 function Activities() {
-  return <p>activities</p>;
+  return (
+    <div className="flex flow-row w-screen h-screen">
+      <div className="bg-orange-500 w-1/2 h-full">Cards</div>
+      <div className="w-1/2">
+        <Map />
+      </div>
+    </div>
+  );
 }
 
 export default Activities;
