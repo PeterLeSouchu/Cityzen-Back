acd55dee733cfeeed3cffe1ecb7bb9c42ee61f81
Goncalves Wilson <will@AirdeGoncalves.lan>
2024-07-03 11:46:10 +0200

    feat:add about page

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,17 @@ import {
 
 import Root from './pages/Roots';
 import './styles/index.css';
+import AboutPage from './pages/AboutPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/about" element={<AboutPage />} />
+    </Route>
+  )
 );
 
 root.render(<RouterProvider router={router} />);
