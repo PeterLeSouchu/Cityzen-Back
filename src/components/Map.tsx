f6d590888f7f5769907b9fde6d21af255179735f
ziad <ziadelidrissi1@gmail.com>
2024-07-05 13:03:24 +0200

    feat: add api data to map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,32 +3,44 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-import marker from '../assets/marker.png';
+import markerIcon from '../assets/marker.png';
+import { useAppSelector } from '../hooks/redux';
 
-const icon = L.icon({
-  iconUrl: marker,
+const customIcon = L.icon({
+  iconUrl: markerIcon,
   iconSize: [25, 38],
 });
 
 const position = [51.505, -0.09];
 
 function Map() {
+  const searched = useAppSelector(
+    (store) => store.activities.searchedActivities
+  );
+
+  const markers = searched.map((activity) => ({
+    geocode: [activity.latitude, activity.longitude],
+    popUp: activity.title,
+  }));
+
+  console.log(markers);
+
   return (
     <MapContainer
       className="w-full h-full"
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={[48.8566, 2.3522]}
+      zoom={5}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=4LWp1keVJZWckLljEDQy"
       />
-      <Marker position={position} icon={icon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {markers.map((marker, index) => (
+        <Marker key={index} position={marker.geocode} icon={customIcon}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
