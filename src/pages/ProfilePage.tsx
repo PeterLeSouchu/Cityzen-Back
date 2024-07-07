459f6c8f71f618cdb4221c95d593ffe388438768
PeterLeSouchu <peter22510@gmail.com>
2024-07-07 12:43:53 +0200

    feat: add responsive on profile-page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -3,15 +3,15 @@ import { Link, Outlet } from 'react-router-dom';
 function ProfilePage() {
   return (
     <div className="w-screen h-80 flex items-center justify-center">
-      <div className="flex items-center border-2 w-9/12   rounded-md h-5/6 overflow-scroll">
-        <section className="w-1/6 min-w-16  border-r-2 flex justify-around items-center flex-col h-full">
-          <Link to="/profile" className="md:text-xl">
+      <div className="flex flex-col md:flex-row items-center border-2 w-9/12   rounded-md h-5/6 overflow-scroll">
+        <section className="md:w-1/6 w-full min-w-16 border-b-2  md:border-r-2 flex flex-row justify-around items-center md:flex-col md:h-full h-24">
+          <Link to="/profile" className=" text-base lg:text-xl">
             Mes informations
           </Link>
           <Link to="/profile/favorites" className=" text-base lg:text-xl">
             Mes favoris
           </Link>
-          <Link to="/profile/my-activities" className="md:text-xl">
+          <Link to="/profile/my-activities" className=" text-base lg:text-xl">
             Mes activités
           </Link>
         </section>
