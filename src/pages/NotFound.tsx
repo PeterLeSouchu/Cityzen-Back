c5f519bb19d9c2a02eca5ffb6f4b840712bceba6
Goncalves Wilson <will@AirdeGoncalves.lan>
2024-07-09 11:12:01 +0200

    feat: add css 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,5 @@
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import header from '../components/Header';
 
 function NotFound() {
   const error = useRouteError();
@@ -20,12 +21,22 @@ function NotFound() {
   }
 
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <p>Désolé, une erreur inattendue est survenue.</p>
-      <p>
-        <i>{getErrorMessage(error)}</i>
-      </p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <div className="text-center">
+        <h1 className="text-9xl font-montserrat text-blue-500">404</h1>
+        <p className="text-lg font-montserrat text-gray-600 mt-2">
+          Désolé, nous n'avons pas pu trouver la page que vous recherchez.
+        </p>
+        <p className="text-md text-gray-500 mt-4">
+          <i>{getErrorMessage(error)}</i>
+        </p>
+        <a
+          href="/"
+          className="bg-green mt-10 inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-600 transition duration-300"
+        >
+          Retour à l'accueil
+        </a>
+      </div>
     </div>
   );
 }
