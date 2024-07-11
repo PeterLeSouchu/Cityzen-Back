ecaf9c144b6e4022411ccd46046a5f00ced1934d
ziadelidrissi <ziadelidrissi1@gmail.com>
2024-07-11 12:15:48 +0200

    chore: adding css to signup/signin popups

--- src/components/Modals/ModalSignup.tsx
+++ src/components/Modals/ModalSignup.tsx
@@ -67,62 +67,133 @@ function ModalSignup({ setModalSignup, setModalSignupOTP }: ModalSignupProps) {
   }
 
   return (
-    <div className=" absolute  w-screen flex justify-center items-center h-screen left-0 top-0">
-      <div className="z-50 w-2/5 min-h-2/5 fixed  bg-gray-300 rounded-md p-4">
-        <button
-          onClick={() => {
-            setModalSignup(false);
-          }}
-          type="button"
-          className="w-full text-right"
-        >
-          Close
-        </button>
+    <div className="absolute w-screen flex justify-center items-center h-screen left-0 top-0">
+      <div className="z-50 w-2/5 min-h-2/5 fixed  bg-lightgrey rounded-md p-4">
+        <div className="flex justify-between items-center mb-4">
+          <span className="font-montserrat text-3xl font-semibold italic">
+            Inscription
+          </span>
+          <button
+            onClick={() => {
+              setModalSignup(false);
+            }}
+            type="button"
+            className="btn btn-circle btn-outline"
+          >
+            <svg
+              aria-label="close"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
         <form onSubmit={(e) => handlerRegister(e)} className="flex flex-col">
-          <div className="flex flex-col">
-            <label htmlFor="pseudo">Pseudo</label>
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="pseudo"
+                className="font-hind text-xl font-semibold"
+              >
+                Pseudo
+              </label>
+            </div>
             <input
               value={pseudo}
               onChange={(e) => handlerPseudo(e)}
               type="text"
               placeholder="Entrez votre pseudo"
               id="pseudo"
+              className="input input-bordered w-full"
             />
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="email">Email</label>
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="email"
+                className="font-hind text-xl font-semibold"
+              >
+                Email
+              </label>
+            </div>
             <input
               value={email}
               onChange={(e) => handlerEmail(e)}
               type="text"
-              placeholder="Entrez votre adresse mail"
+              placeholder="Entrez votre adresse email"
               id="email"
+              className="input input-bordered w-full"
             />
           </div>
-          <div className="flex flex-col">
-            {errorMessage ? <p>{errorMessage}</p> : null}
-            <label htmlFor="password">Mot de passe</label>
+          <div className="flex flex-col mb-4">
+            {errorMessage ? (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{errorMessage}</span>
+              </div>
+            ) : null}
+            <div className="label">
+              <label
+                htmlFor="password"
+                className="font-hind text-xl font-semibold"
+              >
+                Mot de passe
+              </label>
+            </div>
             <input
               value={password}
               onChange={(e) => handlerPassword(e)}
               type="password"
               placeholder="Entrez votre mot de passe"
               id="password"
+              className="input input-bordered w-full"
             />
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="password-confirm">
-              confirmation du mot de passe
-            </label>
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="password-confirm"
+                className="font-hind text-xl font-semibold"
+              >
+                Confirmation du mot de passe
+              </label>
+            </div>
             <input
               value={passwordConfirm}
               onChange={(e) => handlerConfirmPassword(e)}
               type="password"
-              placeholder="Confirmer votre mot de passe"
+              placeholder="Confirmez votre mot de passe"
               id="password-(e) => confirm"
+              className="input input-bordered w-full"
             />
           </div>
-          <button type="submit">Confirmer</button>
+          <button
+            type="submit"
+            className="btn btn-success text-base text-white mt-4 w-1/4 mx-auto"
+          >
+            Confirmer
+          </button>
         </form>
       </div>
     </div>
