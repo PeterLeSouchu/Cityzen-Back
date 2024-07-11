12b38d95f62c429257e2497f7496ff5fa02d1578
ziadelidrissi <ziadelidrissi1@gmail.com>
2024-07-11 14:20:13 +0200

    chore: adding css to activity modificaton popup

--- src/components/Modals/ModalEditActivity.tsx
+++ src/components/Modals/ModalEditActivity.tsx
@@ -18,40 +18,91 @@ function ModalEditActivity({
 
   return (
     <div className=" absolute  w-screen flex justify-center items-center h-screen left-0 top-0">
-      <div className="z-50 w-2/5 h-2/5 fixed  bg-gray-300 rounded-md p-4">
-        <button
-          onClick={() => {
-            setActivityId(null);
-            setModalType(null);
-            console.log(id);
-          }}
-          type="button"
-          className="w-full text-right"
-        >
-          Close
-        </button>
+      <div className="z-50 w-2/5 min-h-2/5 fixed  bg-lightgrey rounded-md p-4">
+        <div className="flex justify-between items-center mb-4">
+          <span className="font-montserrat text-3xl font-semibold italic">
+            Modification d'une activité
+          </span>
+          <button
+            onClick={() => {
+              setActivityId(null);
+              setModalType(null);
+              console.log(id);
+            }}
+            type="button"
+            className="btn btn-circle btn-outline"
+          >
+            <svg
+              aria-label="close"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
         <form className="flex flex-col">
-          <div className="flex flex-col">
-            <label htmlFor="title">Titre</label>
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="title"
+                className="font-hind text-xl font-semibold"
+              >
+                Titre
+              </label>
+            </div>
             <input
               type="text"
               placeholder="Entrez le titre de votre activité"
               id="title"
+              className="input input-bordered w-full"
             />
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="file">Photo (1 seule)</label>
-            <input type="file" placeholder="Ajouter une photo" id="file" />
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="photo"
+                className="font-hind text-xl font-semibold"
+              >
+                Photo (une seule)
+              </label>
+            </div>
+            <input
+              type="file"
+              placeholder="Ajoutez une photo"
+              id="photo"
+              className="input input-bordered w-full"
+            />
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="description">Description</label>
+          <div className="flex flex-col mb-4">
+            <div className="label">
+              <label
+                htmlFor="description"
+                className="font-hind text-xl font-semibold"
+              >
+                Description
+              </label>
+            </div>
             <input
               type="text"
               placeholder="Entrez la description de votre activité"
-              id="description"
+              id="title"
+              className="input input-bordered w-full"
             />
           </div>
-          <button type="submit" onClick={handlerRegister}>
+          <button
+            type="submit"
+            className="btn btn-success text-base text-white mt-4 w-1/4 mx-auto"
+            onClick={handlerRegister}
+          >
             Confirmer
           </button>
         </form>
