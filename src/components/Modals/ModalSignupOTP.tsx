5f78ad91d1ff70b797388bac6bf4175f46837274
ziadelidrissi <ziadelidrissi1@gmail.com>
2024-07-11 13:46:08 +0200

    chore: adding css to otp validation popup

--- src/components/Modals/ModalSignupOTP.tsx
+++ src/components/Modals/ModalSignupOTP.tsx
@@ -46,31 +46,75 @@ function ModalSignupOTP({ setModalSignupOTP, notify }: ModalSignupOTPProps) {
 
   return (
     <div className=" absolute  w-screen flex justify-center items-center h-screen left-0 top-0">
-      <div className="z-50 w-2/5 h-2/5 fixed  bg-gray-300 rounded-md p-4">
-        <button
-          onClick={() => {
-            setModalSignupOTP(false);
-          }}
-          type="button"
-          className="w-full text-right"
-        >
-          Close
-        </button>
+      <div className="z-50 w-2/5 min-h-2/5 fixed  bg-lightgrey rounded-md p-4">
+        <div className="flex justify-between items-center mb-4">
+          <span className="font-montserrat text-3xl font-semibold italic">
+            Validez l'inscription
+          </span>
+          <button
+            onClick={() => {
+              setModalSignupOTP(false);
+            }}
+            type="button"
+            className="btn btn-circle btn-outline"
+          >
+            <svg
+              aria-label="close"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
         <form onSubmit={(e) => handlerRegister(e)} className="flex flex-col">
-          <div className="flex flex-col">
-            {errorMessage ? <p>{errorMessage}</p> : null}
-            <label htmlFor="otp">
-              Entrez le code OTP que vous avez recu par mail
-            </label>
+          <div className="flex flex-col mb-4">
+            {errorMessage ? (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{errorMessage}</span>
+              </div>
+            ) : null}
+            <div className="label">
+              <label htmlFor="code" className="font-hind text-xl font-semibold">
+                Entrez le code OTP que vous avez reçu par email
+              </label>
+            </div>
             <input
               value={code}
               onChange={(e) => handlerCode(e)}
-              type="text"
-              placeholder="Entrez votre code OTP"
-              id="otp"
+              type="code"
+              placeholder="Entrez le code OTP"
+              id="code"
+              className="input input-bordered w-full"
             />
           </div>
-          <button type="submit">Confirmer</button>
+          <button
+            type="submit"
+            className="btn btn-success text-base text-white mt-4 w-1/4 mx-auto"
+          >
+            Confirmer
+          </button>
         </form>
       </div>
     </div>
