9a6b35c89feaf6dffc80cda9b73ff2347468ee3b
Goncalves Wilson <will@AirdeGoncalves.lan>
2024-07-16 09:31:48 +0200

    feat: add css activitypage

--- src/pages/ActivityPage.tsx
+++ src/pages/ActivityPage.tsx
@@ -1,4 +1,9 @@
-import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHeart,
+  faStar,
+  faLocationDot,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
@@ -29,51 +34,60 @@ function ActivityPage() {
 
   return (
     <div className="bg-white py-10 ">
-      <div className="container mx-auto pl-40 p-4">
+      <div className="max-w-4xl mx-auto p-4">
         <div className="flex flex-col md:flex-row gap-2">
           <div className="flex-1 md:flex-2">
             <img
               src={activity.url_image}
               alt={activity.title}
-              className="w-full h-full object-cover rounded-lg"
+              className="w-full h-50 object-cover rounded-lg"
             />
           </div>
         </div>
       </div>
 
-      <div className="pl-40 mt-6">
+      <div className="max-w-4xl mx-auto mt-6 px-4 ">
         <div className="flex items-center justify-between">
-          <h1 className="mx-4 md:mx-8 lg:mx-16 text-3xl font-montserrats text-black">
+          <h1 className="mx-4 md:mx-8 text-3xl font-montserrats text-black">
             {activity.title}
           </h1>
-          <button
-            onClick={() => handlerFavorites(activity.id)}
-            type="button"
-            aria-label="Ajouter / supprimer des favoris"
-          >
-            <FontAwesomeIcon
-              icon={faHeart}
-              className={
-                myFavorites.some(
-                  (favActivity) => favActivity.id === activity.id
-                )
-                  ? 'text-red-500 md:h-6 lg:h-8'
-                  : 'text-slate-200 md:h-6 lg:h-8'
-              }
-            />
-          </button>
-          <div className="badge bg-grey/50 gap-2 md:p-3 lg:p-4 flex mr-80">
-            <FontAwesomeIcon
-              icon={faStar}
-              className="text-yellow-300 md:h-4 lg:h-5"
-            />
-            <span className="font-hind font-semibold text-sm md:text-base lg:text-lg">
-              {activity.avg_rate}
-            </span>
+          <div className="flex items-center gap-5">
+            <button
+              onClick={() => handlerFavorites(activity.id)}
+              type="button"
+              aria-label="Ajouter / supprimer des favoris"
+            >
+              <FontAwesomeIcon
+                icon={faHeart}
+                className={
+                  myFavorites.some(
+                    (favActivity) => favActivity.id === activity.id
+                  )
+                    ? 'text-red-500 md:h-6 lg:h-8'
+                    : 'text-slate-200 md:h-6 lg:h-8'
+                }
+              />
+            </button>
+            <div className="badge bg-grey/50 gap-2 md:p-3 lg:p-4 flex">
+              <FontAwesomeIcon
+                icon={faStar}
+                className="text-yellow-300 md:h-4 lg:h-5"
+              />
+              <span className="font-hind font-semibold text-sm md:text-base lg:text-lg">
+                {activity.avg_rate}
+              </span>
+            </div>
           </div>
         </div>
-
-        <p className="w-full lg:w-2/5 mx-4 md:mx-8 lg:mx-16 mt-4 mb-10 text-black font-montserrat">
+      </div>
+      <div className="max-w-4xl mx-auto px-4">
+        <p className="mx-4 md:mx-8 mt-4 text-black font-montserrat">
+          <FontAwesomeIcon icon={faLocationDot} /> {activity.address}
+        </p>
+        <p className="mx-4 md:mx-8 mt-4  text-black font-montserrat">
+          <FontAwesomeIcon icon={faPhone} /> {activity.phone}
+        </p>
+        <p className="mx-4 md:mx-8 mt-6 mb-10 text-black font-montserrat">
           {activity.description}
         </p>
       </div>
